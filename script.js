c79e270ff76e027db8cb82bc7c63039b293df9cc
hasina.md <mohammadhaseena71@gmail.com>
2024-06-28 14:23:50 +0530

    form validate

--- script.js
+++ script.js
@@ -6,3 +6,23 @@ document.getElementById('action-button').addEventListener('click', function() {
     // changing the background color
     document.body.style.backgroundColor = '#d0e6f5';
 })
+
+// form submission adding event listener
+document.getElementById('hobby-form').addEventListener('submit', function(event) {
+    event.presentDefault();
+    const newHobby = document.getElementById('new-hobby').ariaValueMax.trim();
+    console.log('new hobby:', newHobby);
+    //validate input
+    if (newHobby) {
+        const newListItem = document.createElement('li');
+        newListItem.textContent = newHobby;
+        // adding new list item to hobby list
+        document.getElementById('hobby-list').appendChild(newListItem);
+        // clear input
+        document.getElementById('new-hobby').value='';
+    }
+    else {
+        alert('Please enter a hobby');
+    }
+
+});
